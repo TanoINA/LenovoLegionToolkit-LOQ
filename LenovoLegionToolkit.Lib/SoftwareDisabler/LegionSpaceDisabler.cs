4bc1b7a8102c7201cedaf595e9ae68481e6c12c6
Tano <90976970+TanoINA@users.noreply.github.com>
2026-09-07 21:34:15 +0700

    fix(power): harden power adapter state detection against transient ACLineStatus 255 and hybrid battery discharge spikes

--- LenovoLegionToolkit.Lib/SoftwareDisabler/LegionSpaceDisabler.cs
+++ LenovoLegionToolkit.Lib/SoftwareDisabler/LegionSpaceDisabler.cs
@@ -1,10 +1,28 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace LenovoLegionToolkit.Lib.SoftwareDisabler;
 
 public class LegionSpaceDisabler : AbstractSoftwareDisabler
 {
     protected override IEnumerable<string> ScheduledTasksPaths => [];
-    protected override IEnumerable<string> ServiceNames => ["DAService"];
-    protected override IEnumerable<string> ProcessNames => ["LegionSpace", "LSDaemon"];
+    protected override IEnumerable<string> ServiceNames =>
+    [
+        "DAService",
+        "LenovoSmartService",
+        "GAService"
+    ];
+
+    protected override IEnumerable<string> ProcessNames =>
+    [
+        "LegionSpace",
+        "LSDaemon",
+        "SmartEngineHost64",
+        "SmartEngineHostN64",
+        "SmartEngineHostS64",
+        "LenovoSmartService",
+        "GAService",
+        "GAController",
+        "GAWorker",
+        "seworker"
+    ];
 }
